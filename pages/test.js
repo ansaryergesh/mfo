0b0d7f0974bc7de5137efad23578b18df3ec92e9
ansaryergesh <yergeshovansar@gmail.com>
2020-11-11 10:47:08 +0600

    first commit

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Test() {
+  return (
+    <div >
+      Test Page
+      Test Page
+      Test Page
+      Test Page
+      Test Page
+      Test Page
+    </div>
+  )
+}
