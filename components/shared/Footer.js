5162ce51fffe9e3ee993949d8a37311fcd0056a0
ansaryergesh <yergeshovansar@gmail.com>
2020-11-12 11:52:30 +0600

    just check

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
     <div className="container text-center">
-      <small>Copyright &copy; Your Website</small>
+      <small>Copyright &copy; Your</small>
     </div>
   </footer>
   )
