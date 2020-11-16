cf2f4e9f0290150b07a8dbd75c358f90816ac3c6
ansaryergesh <yergeshovansar@gmail.com>
2020-11-16 11:26:36 +0600

    check1

---
+++ out/js/myscript.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "UA-176299351-1");
+  gtag("config", "AW-599429567");
+  gtag("config", "AW-599429567/GuV9CPH39twBEL-j6p0C", {
+    phone_conversion_number: "87272501500",
+});
