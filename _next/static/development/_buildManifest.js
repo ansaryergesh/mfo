fd05749064bbfdf967d8122c9c3abea84811e101
Ansar <yergeshovansar@gmail.com>
2021-02-19 17:34:25 +0600

    saled

--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cabinet/loans":["static\u002Fchunks\u002Fpages\u002Fcabinet\u002Floans.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcabinet\u002Floans","\u002Flogin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
