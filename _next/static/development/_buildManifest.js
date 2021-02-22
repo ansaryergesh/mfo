2a82ff9ae8f0aa3fa5a7601500a5ff5b6fb4fd53
Ansar <yergeshovansar@gmail.com>
2021-02-22 16:39:13 +0600

    last child doc show

--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/404":["static\u002Fchunks\u002Fpages\u002F404.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/aggrements":["static\u002Fchunks\u002Fpages\u002Faggrements.js"],sortedPages:["\u002F404","\u002F_app","\u002F_error","\u002Faggrements"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
